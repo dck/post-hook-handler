fea38786ff139e3cd266186886ec1aad60d60b09
Denis Usanov <usanovdd@gmail.com>
2014-10-27 19:41:30 +0300

    Implemented 1st version

--- hook-handler.py
+++ hook-handler.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import json
 import sh
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
+from urlparse import parse_qs
 
+WORK_BRANCH = "default"
 REPOS_PATHS = {
-    "repo": "~/work/repo"
+    "SOPT": "/tmp/aaa/sopt"
 }
 
 class Error(Exception): pass
 class SCMDoesntSupport(Error): pass
+class BadRequest(Error): pass
+class NoPathForRepo(Error): pass
 
 class SCMHandler(object):
 
@@ -39,20 +46,7 @@ class GitHandler(SCMHandler):
     def checkout(self, branchname):
         sh.git.checkout(branchname)
 
-def scm_factory(scm_name, *args, **kwargs):
-    d = {
-        "hg": HgHandler,
-        "git": GitHandler
-    }
-    try:
-        creator = d[scm_name]
-    except KeyError:
-        raise SCMDoesntSupport(scm_name)
-
-    return creator(*args, **kwargs)
-
-
-class RepoInfo(object):
+class PushInfo(object):
 
     def __init__(self, json_payload):
         obj_payload = json.loads(json_payload)
@@ -68,6 +62,76 @@ class RepoInfo(object):
     def is_branch_changed(self, branchname):
         res = any(c["branch"] == branchname for c in self.commits)
         return res
+
+
+
+class Handler(BaseHTTPRequestHandler):
+
+    def parse_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers['content-type'])
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers['content-length'])
+            postvars = parse_qs(
+                    self.rfile.read(length),
+                    keep_blank_values=1)
+        else:
+            postvars = {}
+        return postvars
+
+    def do_POST(self):
+        postvars = self.parse_POST()
+        try:
+            payload = postvars["payload"][0]
+        except KeyError:
+            raise BadRequest
+
+        go(payload)
+
+
+def scm_factory(scm_name, *args, **kwargs):
+    d = {
+        "hg": HgHandler,
+        "git": GitHandler
+    }
+    try:
+        creator = d[scm_name]
+    except KeyError:
+        raise SCMDoesntSupport(scm_name)
+
+    return creator(*args, **kwargs)
+
+
+def go(payload):
+    print "Got POST request"
+    r = PushInfo(payload)
+    if not r.is_branch_changed(WORK_BRANCH):
+        print "There are no proper changes in {}".format(r.name())
+        return
+
+    try:
+       path = REPOS_PATHS[r.name()]
+       print "Path to work: {}".format(path)
+    except KeyError:
+        raise NoPathForRepo
+
+    print "SCM: {}".format(r.scm())
+    scm_handler = scm_factory(r.scm())
+    old_path = os.getcwd()
+    try:
+        sh.cd(path)
+        scm_handler.checkout(WORK_BRANCH)
+        scm_handler.pull()
+        print "Complete"
+    finally:
+        sh.cd(old_path)
+
+
+server = HTTPServer(('', 4444), Handler)
+print "Start serving"
+server.serve_forever()
+
 
 
 # {
